f5caf914ca17a153e717f21ae1347e655947deaf
Mancini6i <27mancisa@isd748.org>
2025-05-01 23:57:27 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Kitchen Equipment Conquest!</h1>
+      <p>The game is under construction.</p>
+    </div>
+  );
+}
+
+export default App;
