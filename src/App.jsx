3d9aa58b5e5387d36b0d1aa429f968c65011c6a9
Mancini6i <27mancisa@isd748.org>
2025-05-02 14:03:08 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kitchen Equipment Conquest Game</h1>
+      <p>Game coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
