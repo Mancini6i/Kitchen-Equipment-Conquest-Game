435809a1167a7a9fdf8e8a30e4b156bbefb52db7
Mancini6i <27mancisa@isd748.org>
2025-05-01 23:56:40 -0500

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import KitchenGame from './KitchenGame';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <KitchenGame />
-  </React.StrictMode>
-);
